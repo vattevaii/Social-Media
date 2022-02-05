f5e12f9646a8e7cbe418f8b7c4d3fabba7346f5d
Vattevaii <vattevaii@gmail.com>
2022-02-05 16:09:12 +0545

    After Build

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-   baseURL: "http://localhost:5000/api",
+   baseURL: "http://192.168.1.65:5000/api",
+   // baseURL: "http://localhost:5000/api",
    headers: {
       "Content-type": "application/json",
       "Access-Control-Allow-Origin": "http://localhost:5000",
