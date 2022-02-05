ecc38adb3e1ba795d5cd3234d51862d04a7a52d3
Vattevaii <vattevaii@gmail.com>
2022-02-05 23:52:32 +0545

    update backend baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-   baseURL: "http://192.168.1.65:5000/api",
+   // baseURL: "http://192.168.1.65:5000/api",
    // baseURL: "http://localhost:5000/api",
+   baseURL: "https://social-media245.herokuapp.com/api",
    headers: {
       "Content-type": "application/json",
       "Access-Control-Allow-Origin": "http://localhost:5000",
