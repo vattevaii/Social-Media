39c4a467fbc5aec57132562d3e62d8954d81a0fd
Vattevaii <vattevaii@gmail.com>
2022-02-10 19:11:00 +0545

    Cookie not working still

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import apiClient from "./http-common"
 import { useMutation } from "react-query";
 import RouteingLogic from './Router';
 import MessageToast from './experiment/MessagesToast/Toast';
-// import useActions from './context/useActions';
+import useActions from './context/useActions';
 
 function App() {
   const { dispatch, accessToken, user } = useContext(AuthContext);
   const [cookies] = useCookies()
-
+  const { foundError } = useActions();
   // const { foundError } = useActions(AuthContext);
   const tryPosts = useMutation("tryPosts", () => {
     apiClient.get("/posts/timeline", { params: { email: user.email } })
       .then((data) => { console.log(data); })
-      .catch((err) => { console.log(err.response.statusText) });
+      .catch((err) => { foundError(err) });
   });
   // const navigate = useNavigate();
   useEffect(() => {
