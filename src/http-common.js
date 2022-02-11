27ca22dd720852576fa153efd6c971d7362cfea6
Vattevaii <vattevaii@gmail.com>
2022-02-11 11:33:57 +0545

    Still

--- src/http-common.js
+++ src/http-common.js
@@ -2,9 +2,7 @@ import axios from "axios";
 console.log(process.env.REACT_APP_BACKEND)
 axios.defaults.withCredentials = true;
 export default axios.create({
-   // baseURL: "http://192.168.1.65:5000/api",
-   // baseURL: "http://localhost:5000/api",
-   baseURL: process.env.REACT_APP_BACKEND + "/api",
+   baseURL: "/api",
    headers: {
       "Content-type": "application/json",
       "Access-Control-Allow-Origin": process.env.REACT_APP_BACKEND,
